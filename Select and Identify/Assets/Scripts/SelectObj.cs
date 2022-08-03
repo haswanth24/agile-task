6dd18b4e314252c4f55fa319beadadf6df7c8164
haswanth24 <haswanth.kasireddy@st.ovgu.de>
2022-08-03 09:16:02 +0200

    change material

--- Select and Identify/Assets/Scripts/SelectObj.cs
+++ Select and Identify/Assets/Scripts/SelectObj.cs
@@ -1,18 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class SelectObj : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private string selectTag = "Select";
+    [SerializeField] private Material solidMaterial;
+
+    private Material defMaterial;
+    private GameObject clicked = null;
+    private GameObject prevClicked= null;
+    private Renderer selectRend;
+    private Renderer prevRend;
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+   
     void Update()
     {
+        if(Input.GetMouseButtonDown(0))
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 400f))
+            {
+                clicked = hit.transform.gameObject;
+                
+                if(clicked.CompareTag(selectTag))
+                {
+                    if (clicked != prevClicked)
+                    {
+                        if(prevClicked !=null)
+                        {
+                            prevRend = prevClicked.GetComponent<Renderer>();
+                            prevRend.material = defMaterial;
+                        }
+
+                        selectRend = clicked.GetComponent<Renderer>();
+                        defMaterial = selectRend.material;
+                        selectRend.material=solidMaterial;
+
+                        prevClicked = clicked;
         
+                    }
+                }
+            }
+        }
     }
 }

08d36732c48ecd8982e8c9a7e692f8c9e065aa15
haswanth24 <haswanth.kasireddy@st.ovgu.de>
2022-08-03 10:04:42 +0200

    text and translate

--- Select and Identify/Assets/Scripts/SelectObj.cs
+++ Select and Identify/Assets/Scripts/SelectObj.cs
@@ -14,9 +14,21 @@ public class SelectObj : MonoBehaviour
     private GameObject prevClicked= null;
     private Renderer selectRend;
     private Renderer prevRend;
+    [SerializeField] private GameObject textSphere;
+    [SerializeField] private GameObject textCube;
+
+    public GameObject Sphere;
+    public GameObject Cube;
+    private Vector3 defPosSphere;
+    private Vector3 defPosCube;
+    private Vector3 newPosSphere;
+    private Vector3 newPosCube;
     void Start()
     {
-        
+        defPosSphere=Sphere.transform.position;
+        Vector3 newPosSphere=defPosSphere;
+        defPosCube=Cube.transform.position;
+        Vector3 newPosCube=defPosCube;
     }
 
    
@@ -26,12 +38,38 @@ public class SelectObj : MonoBehaviour
         {
             var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
-            if (Physics.Raycast(ray, out hit, 400f))
+            if (Physics.Raycast(ray, out hit, 300f))
             {
                 clicked = hit.transform.gameObject;
                 
                 if(clicked.CompareTag(selectTag))
                 {
+
+                    if (hit.collider.name =="Sphere")
+                    {
+                        textSphere.SetActive(true);
+                        textCube.SetActive(false);
+
+                        defPosSphere= Sphere.transform.position;
+                        Vector3 newPosSphere= defPosSphere;
+                        newPosSphere.y = +1.5f;
+                        Sphere.transform.position=newPosSphere;
+
+                        Cube.transform.position=defPosCube;
+                    }
+
+                    else if(hit.collider.name=="Cube")
+                    {
+                        textCube.SetActive(true);
+                        textSphere.SetActive(false);
+
+                        defPosCube = Cube.transform.position;
+                        Vector3 newPosCube=defPosCube;
+                        newPosCube.y = +1.5f;
+                        Cube.transform.position= newPosCube;
+
+                        Sphere.transform.position= defPosSphere;
+                    }
                     if (clicked != prevClicked)
                     {
                         if(prevClicked !=null)
